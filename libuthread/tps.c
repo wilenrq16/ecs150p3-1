bac3df777d6d3375d69e96a0dcf1c00d6e7b7ac7
wilenrq16 <35649364+wilenrq16@users.noreply.github.com>
2019-05-15 23:12:53 -0700

    Update tps.c

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,34 +12,36 @@
 #include "thread.h"
 #include "tps.h"
 
-typedef struct page {
-    int reference_count;
-    void* addr;
-} page;
+queue_t TPSqueue;
 
-typedef struct tps {
-    page* tps_page;
-    pthread_t tid;
-} tps;
+typedef struct memPage{
+  int reference;
+  void *page;
+}memPage_t;
 
-queue_t tps_queue = NULL;
+typedef struct tps{
+  pthread_t tid;
+  memPage_t *tpsPage;
+}tps_t;
 
-//searches for tps by comparing tid
-static int find_tps(void* data, void* argv) {
-  pthread_t tid = *((pthread_t*)argv);
+/*HELPER FUNCTIONS*/
+int tps_find(void* tps, void* arg)
+{
+	
+  pthread_t tid = *((pthread_t*)arg);
 
-  if(((tps*)data)->tid == tid) {
+  if(((tps_t*)tps)->tid == tid) {
     return 1;
   }
   return 0;
 }
 
-//searches for page by comparing address
-static int find_addr(void* data, void* argv)
+int page_find(void* tps, void* arg)
 {
-  void* addr = argv;
+	
+  void* page = arg;
 
-  if(((tps*)data)->tps_page->addr == addr) {
+  if(((tps_t*)tps)->tpsPage->page == page) {
     return 1;
   }
   return 0;
@@ -47,23 +49,25 @@ static int find_addr(void* data, void* argv)
 
 static void segv_handler(int sig, siginfo_t *si, void *context)
 {
-    tps* my_tps = NULL;
     /*
      * Get the address corresponding to the beginning of the page where the
      * fault occurred
      */
-    void *p_fault = (void*)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
 
+    
+    void *p_fault = (void*)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
+    
     /*
      * Iterate through all the TPS areas and find if p_fault matches one of them
      */
-
-    queue_iterate(tps_queue, find_addr, p_fault, (void**)&my_tps);
-
-    if (my_tps != NULL)
+	 struct memPage *page = NULL;	    
+	 queue_iterate(TPSqueue, page_find, p_fault, (void**)&page);
+   
     /* Printf the following error message */
+    if (page != NULL){
         fprintf(stderr, "TPS protection error!\n");
-
+    }
+	
     /* In any case, restore the default signal handlers */
     signal(SIGSEGV, SIG_DFL);
     signal(SIGBUS, SIG_DFL);
@@ -75,65 +79,49 @@ static void segv_handler(int sig, siginfo_t *si, void *context)
 
 int tps_init(int segv)
 {
-  //check if init has already been called by seeing if queue has been created
-  if (tps_queue != NULL) {
-      return -1;
+  enter_critical_section();
+  if (queue_length(TPSqueue) != -1){ //When the tps api has been initialized
+    return -1;
   }
 
-  //create queue
-  tps_queue = queue_create();
-  if (tps_queue == NULL) {
-      return -1;
-  }
+  //initializing Q
+  TPSqueue = queue_create();
+  exit_critical_section();
 
-  //set segfault handler if desired by user
   if (segv) {
     struct sigaction sa;
-
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = segv_handler;
     sigaction(SIGBUS, &sa, NULL);
     sigaction(SIGSEGV, &sa, NULL);
   }
-
   return 0;
 }
 
 int tps_create(void)
 {
-  pthread_t cur_tid = pthread_self();
-  tps* my_tps = NULL;
-
-  //search for tps block
+  tps_t *newtps = NULL;
+  pthread_t tidHolder = pthread_self();
+	
   enter_critical_section();
-  queue_iterate(tps_queue, find_tps, (void*)&cur_tid, (void**)&my_tps);
+  queue_iterate(TPSqueue, tps_find, (void*)tidHolder, (void**)&newtps);
   exit_critical_section();
-  //return -1 if tps already exists
-  if (my_tps != NULL) {
-    return -1;
+	
+  if (newtps != NULL){
+	 return -1;
   }
-  //create tps block
-  my_tps = malloc(sizeof(tps));
-  if (my_tps == NULL) {
-    return -1;
+	
+  newtps = malloc(sizeof(struct tps));
+  newtps->tid = tidHolder;
+  newtps->tpsPage->page = mmap(NULL, TPS_SIZE ,PROT_NONE,MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  newtps->tpsPage->reference = 1;
+  if (newtps == NULL || newtps->tpsPage == NULL ||  newtps->tpsPage->page == MAP_FAILED){
+	  return -1;
   }
-  //create page struct
-  my_tps->tps_page = malloc(sizeof(page));
-  if (my_tps->tps_page == NULL) {
-    return -1;
-  }
-  //create page
-  my_tps->tps_page->addr = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, 10, 0);
-  if (my_tps->tps_page->addr == MAP_FAILED) {
-    return -1;
-  }
-  //save tid and set reference_count to 1
-  my_tps->tid = cur_tid;
-  my_tps->tps_page->reference_count = 1;
-  //add to queue
-  enter_critical_section();
-  queue_enqueue(tps_queue, (void*)my_tps);
+	
+  enter_critical_section();	
+  queue_enqueue(TPSqueue, newtps);
   exit_critical_section();
 
   return 0;
@@ -141,128 +129,136 @@ int tps_create(void)
 
 int tps_destroy(void)
 {
-  pthread_t cur_tid = pthread_self();
-  tps* my_tps = NULL;
-  //search for tps in queue
+  pthread_t tid= pthread_self();
+  tps_t *tps = NULL;
+	
   enter_critical_section();
-  queue_iterate(tps_queue, find_tps, (void*)&cur_tid, (void**)&my_tps);
-  exit_critical_section();
-  //if not found fail
-  if (my_tps == NULL) {
-      return -1;
+  queue_iterate(TPSqueue, tps_find, (void*)&tid, (void**)&tps);
+  	
+  if (tps != NULL){
+    exit_critical_section();
+	 return -1;
   }
-  //delete from queue
-  enter_critical_section();
-  queue_delete(tps_queue, (void*) my_tps);
+
+  munmap(tps->tpsPage->page, TPS_SIZE);
+  free(tps->tpsPage);
+
+  queue_delete(TPSqueue, tps);
   exit_critical_section();
 
-  //if more than one tps points to page, deincrement reference_count and free tps block
-  if (my_tps->tps_page->reference_count > 1) {
-      my_tps->tps_page->reference_count --;
-  }
-  //else free page and then free tps block
-  else if (my_tps->tps_page->reference_count == 1) {
-      munmap(my_tps->tps_page->addr, TPS_SIZE);
-      free(my_tps->tps_page);
-  }
-    free(my_tps);
   return 0;
 }
 
 int tps_read(size_t offset, size_t length, char *buffer)
 {
-  pthread_t cur_tid = pthread_self();
-  tps* my_tps = NULL;
-  //find tps in queue
+	/* TODO: Phase 2 */
+   tps_t *newtps = NULL;
+  pthread_t tidHolder = pthread_self();
+  //run without getting interrupted
   enter_critical_section();
-  queue_iterate(tps_queue, find_tps, (void*)&cur_tid, (void**)&my_tps);
-  exit_critical_section();
-  //check for valid tps block and read info
-  if (my_tps == NULL || offset + length > TPS_SIZE || buffer == NULL) {
+  queue_iterate(TPSqueue, tps_find, (void*)tidHolder, (void**)&newtps);
+  //lower the security to be able to read
+  if (newtps == NULL || (offset + length) > TPS_SIZE || buffer == NULL) {
+    exit_critical_section();
     return -1;
   }
-  //change protections and read
-  mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_READ);
-  memcpy((void*)buffer, my_tps->tps_page->addr + offset, length);
-  mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_NONE);
+  int error;
+  error = mprotect(newtps->tpsPage->page, TPS_SIZE, PROT_READ);
+
+  if (error != 0 ){
+    exit_critical_section();
+    return -1;
+  }
+  //transfering data
+  memcpy(buffer, newtps->tpsPage->page, offset);
+  //The memory cannot be accessed at all
+  //mprotect(newtps->memPage, TPS_SIZE, PROT_NONE);
+
+  error = mprotect(newtps->tpsPage->page, TPS_SIZE, PROT_NONE);
+  if (error != 0 ){
+    return -1;
+  }
+  exit_critical_section();
+
   return 0;
 }
 
 int tps_write(size_t offset, size_t length, char *buffer)
 {
-  pthread_t cur_tid = pthread_self();
-  tps* my_tps = NULL;
-  //find tps in queue
+	/* TODO: Phase 2 */
+
+  tps_t *newtps = NULL;
+  pthread_t tidHolder = pthread_self();
+
   enter_critical_section();
-  queue_iterate(tps_queue, find_tps, (void*)&cur_tid, (void**)&my_tps);
-  exit_critical_section();
-  //check for valid tps block and read info
-  if (my_tps == NULL || offset + length > TPS_SIZE || buffer == NULL){
+  queue_iterate(TPSqueue, tps_find, (void*)tidHolder, (void**)&newtps);
+
+  if (newtps == NULL || (offset + length) > TPS_SIZE || buffer == NULL) {
     return -1;
   }
 
   enter_critical_section();
-  //if more than one reference, copy page then write
-  if (my_tps->tps_page->reference_count > 1) {
+  if (newtps->tpsPage->reference > 1) {
     //make space for new page
-    page* new_page = malloc(sizeof(page));
-    new_page->addr = mmap(NULL, TPS_SIZE, PROT_WRITE, MAP_PRIVATE | MAP_ANON, 10, 0);
-    if (new_page->addr == MAP_FAILED) {
-      return -1;
-    }
-    new_page->reference_count = 1;
+    memPage_t* newPage = malloc(sizeof(memPage_t));
+    newPage->page = mmap(NULL, TPS_SIZE, PROT_WRITE, MAP_PRIVATE | MAP_ANON, 10, 0);
+  
+    newPage->reference= 1;
+    
+    mprotect(newtps->tpsPage->page, TPS_SIZE, PROT_READ);
+    memcpy(newtps->tpsPage, newtps->tpsPage->page, TPS_SIZE);
+    mprotect(newtps->tpsPage->page, TPS_SIZE, PROT_NONE);
+    mprotect(newtps->tpsPage, TPS_SIZE, PROT_NONE);
+    newtps->tpsPage->reference --;
+    newtps->tpsPage = newPage;
+  }
+  int errorWrite;
+  
+  errorWrite = mprotect(newtps->tpsPage->page, TPS_SIZE, PROT_WRITE);
 
-    //change permissions and copy page
-    mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_READ);
-    memcpy(new_page->addr, my_tps->tps_page->addr, TPS_SIZE);
-    mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_NONE);
-    mprotect(new_page->addr, TPS_SIZE, PROT_NONE);
-    my_tps->tps_page->reference_count --;
-    my_tps->tps_page = new_page;
+  if (errorWrite != 0 ){
+    return -1;
+  }
+
+  memcpy(newtps->tpsPage->page, buffer, offset);
+
+  errorWrite = mprotect(newtps->tpsPage->page, TPS_SIZE, PROT_NONE);
+  if (errorWrite != 0 ){
+
+    return -1;
   }
   exit_critical_section();
 
-  //change permissions and write
-  mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_WRITE);
-  memcpy( my_tps->tps_page->addr + offset, (void*)buffer, length);
-  mprotect(my_tps->tps_page->addr, TPS_SIZE, PROT_NONE);
   return 0;
 }
 
 int tps_clone(pthread_t tid)
 {
-  tps* target_tps = NULL;
-  tps* my_tps = NULL;
-  pthread_t cur_tid = pthread_self();
+  tps_t* targettps = NULL;
+  tps_t* clonetps = NULL;
+  pthread_t clonetid = pthread_self();
 
-  //seach for target tps in queue
   enter_critical_section();
-  queue_iterate(tps_queue, find_tps, (void*)&tid, (void**)&target_tps);
-  exit_critical_section();
-  if (target_tps == NULL) {
+  queue_iterate(TPSqueue, tps_find, (void*)&tid, (void**)&targettps);
+  if (targettps == NULL) {
+    exit_critical_section();
     return -1;
   }
-  //search for current tps in queue
-  enter_critical_section();
-  queue_iterate(tps_queue, find_tps, (void*)&cur_tid, (void**)&my_tps);
+  queue_iterate(TPSqueue, tps_find, (void*)&clonetid, (void**)&clonetps);
   exit_critical_section();
-  if (my_tps != NULL) {
+  if (clonetps != NULL) {
+    exit_critical_section();
     return -1;
   }
 
-  //make new tps block
-  my_tps = malloc(sizeof(tps));
-  if (my_tps == NULL) {
-    return -1;
-  }
-  //update reference_count
-  enter_critical_section();
-  my_tps->tps_page = target_tps->tps_page;
-  my_tps->tps_page->reference_count ++;
+  clonetps = malloc(sizeof(tps_t));  
+  clonetps->tid = clonetid;
+  clonetps->tpsPage = clonetps->tpsPage;
+  clonetps->tpsPage->reference++;
+  
 
-  my_tps->tid = cur_tid;
-  queue_enqueue(tps_queue, (void*)my_tps);
+  queue_enqueue(TPSqueue, (void*)clonetps);
   exit_critical_section();
 
-  return 0;
+return 0;
 }
